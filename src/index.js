763be019846d6feb86cc450aa4d07cf0e206a0e9
oleg nechaev <nechaevon2105@gmail.com>
2019-12-09 00:37:42 +0300

    homework_6 done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 /* ДЗ 6 - Асинхронность и работа с сетью */
 
 /*
@@ -9,6 +10,8 @@
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
 function delayPromise(seconds) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), seconds * 1000);
 }
 
 /*
@@ -25,6 +28,19 @@ function delayPromise(seconds) {
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
 function loadAndSortTowns() {
+    return new Promise((resolve, reject) => {
+        fetch('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json')
+            .then(resp => {
+                return resp.json();
+            })
+            .then(towns => {
+                let sortTowns = towns.sort((a, b) => {
+                return a.name > b.name ? 1 : -1;
+            })
+            resolve(sortTowns);
+            })
+            .catch((e) => reject(e));
+    })
 }
 
 export {
