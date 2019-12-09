8b6b14e0d985dede234ff2cc32f8ed923998af0e
oleg nechaev <nechaevon2105@gmail.com>
2019-12-09 16:03:43 +0300

    homework_7_done

---
+++ src/cookie.js
@@ -0,0 +1,121 @@
+/*
+ ДЗ 7 - Создать редактор cookie с возможностью фильтрации
+
+ 7.1: На странице должна быть таблица со списком имеющихся cookie. Таблица должна иметь следующие столбцы:
+   - имя
+   - значение
+   - удалить (при нажатии на кнопку, выбранная cookie удаляется из браузера и таблицы)
+
+ 7.2: На странице должна быть форма для добавления новой cookie. Форма должна содержать следующие поля:
+   - имя
+   - значение
+   - добавить (при нажатии на кнопку, в браузер и таблицу добавляется новая cookie с указанным именем и значением)
+
+ Если добавляется cookie с именем уже существующей cookie, то ее значение в браузере и таблице должно быть обновлено
+
+ 7.3: На странице должно быть текстовое поле для фильтрации cookie
+ В таблице должны быть только те cookie, в имени или значении которых, хотя бы частично, есть введенное значение
+ Если в поле фильтра пусто, то должны выводиться все доступные cookie
+ Если добавляемая cookie не соответсвует фильтру, то она должна быть добавлена только в браузер, но не в таблицу
+ Если добавляется cookie, с именем уже существующей cookie и ее новое значение не соответствует фильтру,
+ то ее значение должно быть обновлено в браузере, а из таблицы cookie должна быть удалена
+
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+ */
+
+/*
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер
+
+ Пример:
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
+ */
+const homeworkContainer = document.querySelector('#homework-container');
+// текстовое поле для фильтрации cookie
+const filterNameInput = homeworkContainer.querySelector('#filter-name-input');
+// текстовое поле с именем cookie
+const addNameInput = homeworkContainer.querySelector('#add-name-input');
+// текстовое поле со значением cookie
+const addValueInput = homeworkContainer.querySelector('#add-value-input');
+// кнопка "добавить cookie"
+const addButton = homeworkContainer.querySelector('#add-button');
+// таблица со списком cookie
+const listTable = homeworkContainer.querySelector('#list-table tbody');
+
+filterNameInput.addEventListener('keyup', function() {
+    // здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
+    appendCookie();
+});
+
+addButton.addEventListener('click', () => {
+    // здесь можно обработать нажатие на кнопку "добавить cookie"
+    createCookie(addNameInput.value, addValueInput.value);
+    appendCookie();
+});
+
+listTable.addEventListener('click', removeCookie);
+
+function createRowCookie(name, value) {
+    let tr = document.createElement('tr');
+
+    tr.innerHTML = '<td>'+name+'</td><td>'+value+'</td><td><button>Удалить</button></td>';
+    
+    return tr;
+}
+
+function createCookie(name, value) {
+    document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+}
+
+function getCookies() {
+    return document.cookie.split(';').reduce((t, s) => Object.assign(t, { [s.split('=')[0]]: s.split('=')[1] }), {});
+}
+
+function appendCookie() {
+    let cookies = getCookies();
+
+    if (filterNameInput.value === '') {
+        allCookies();
+    } else if (filterNameInput.value !== '') {
+        clearTable();
+       
+        for (let cookie in cookies) {
+            if ({}.hasOwnProperty.call(cookies, cookie)) {
+                if (isMatching(cookie, filterNameInput.value) || isMatching(cookies[cookie], filterNameInput.value)) { 
+                    listTable.appendChild(createRowCookie(cookie, cookies[cookie])); 
+                } 
+            } 
+        }
+    }
+}
+
+function clearTable() {
+    while (listTable.firstChild) {
+        listTable.removeChild(listTable.firstChild);
+    }
+}
+
+function removeCookie(event) {
+    if (event.target.nodeName === 'BUTTON') {
+        let tr = event.target.parentNode.parentNode;
+
+        document.cookie = event.target.parentNode.parentNode.firstChild.innerText+'=; max-age=0';
+        tr.remove();
+    } 
+}
+
+function isMatching(search, chunk) {
+    return search.toUpperCase().includes(chunk.toUpperCase());
+}
+
+function allCookies() {
+    clearTable();
+    let cookies = getCookies();
+
+    for (let cookie in cookies) {
+        if ({}.hasOwnProperty.call(cookies, cookie)) {
+            listTable.appendChild(createRowCookie(cookie, cookies[cookie]));
+        }
+    }
+}
